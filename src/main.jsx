0af5bda850ba4d96a31b2db600b702dee91f0786
Jose Rangel <rangelreyesjose.jarr@gmail.com>
2026-04-11 18:08:09 -0700

    feat: Update main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from './App.jsx';
 import './index.css'; // Importamos nuestra hoja de estilos limpia
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
